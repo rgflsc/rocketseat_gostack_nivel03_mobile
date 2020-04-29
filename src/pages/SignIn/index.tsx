7382e73d8daa36263b45096089bbf4f33a6b63a1
Rodrigo Garcia <rgflsc@gmail.com>
2020-04-29 17:10:58 -0300

    Tela de autenticação
    Tela de autenticação

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,25 +1,50 @@
 import React from 'react'
-import { Image } from 'react-native'
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
 
 import logoImg from '../../assets/logo.png';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles'
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles'
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg}></Image>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg}></Image>
 
-      <Title>Faça seu logon</Title>
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button onPress={() => { console.log('dfg') }} >Entrar</Button>
-    </Container>
+            <Button onPress={() => { console.log('dfg') }} >Entrar</Button>
+
+            <ForgotPassword onPress={() => { console.log('sdf') }}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => { console.log('sdf') }}>
+        <Icon name={'log-in'} size={20} color="#ff9000" />
+        <CreateAccountButtonText>Cria uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   )
 }
 
