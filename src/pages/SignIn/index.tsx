324a71ff0f02d5e2b2e1991fa61dd73582303bbc
Rodrigo Garcia <rgflsc@gmail.com>
2020-04-29 23:35:23 -0300

    Cadastro na aplicação
    Cadastro na aplicação

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,13 @@
 import React, { useCallback, useRef } from 'react'
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput } from 'react-native'
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput, Alert } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
 import { Form } from '@unform/mobile'
 import { FormHandles } from '@unform/core'
+import * as Yup from 'yup';
+import api from '../../services/api'
+
+import getValidationErros from '../../utils/getValidationErros';
 
 import logoImg from '../../assets/logo.png';
 
@@ -12,15 +16,56 @@ import Button from '../../components/Button';
 
 import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles'
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
 
   const navigation = useNavigation();
 
-  const handleSignIn = useCallback((data: object) => {
-    console.log(data);
-  }, []);
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        //await signIn({
+        //  email: data.email,
+        //  password: data.password,
+        //});
+
+        //history.push('/dashboard');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErros(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        Alert.alert(
+          'Erro na autenticação',
+          'Ocorreu um erro ao fazer o login, cheque as credenciais.',
+        );
+      }
+    },
+    [],
+  );
 
   return (
     <>
@@ -40,7 +85,7 @@ const SignIn: React.FC = () => {
               <Title>Faça seu logon</Title>
             </View>
 
-            <Form ref={formRef} onSubmit={handleSignIn}>
+            <Form ref={formRef} onSubmit={handleSubmit}>
               <Input
                 autoCorrect={false}
                 autoCapitalize="none"
